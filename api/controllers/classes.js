49b2a89e02f449b29bfcdfbc053d0fd2dbbaa8bb
Haymee <hmpark0502@gmail.com>
2018-10-04 22:14:32 +0900

    class api update

--- api/controllers/classes.js
+++ api/controllers/classes.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 const util = require('util');
 const { Class } = require('../../models');
 
@@ -9,29 +11,75 @@ module.exports = {
 };
 
 function getClasses(req, res) {
-    const name = req.swagger.params.name.value || 'BB';
-    const hello = util.format('Hello, %s', name);
+    Class.findAndCountAll({
+        where: {deleted_dt: null}
+    }).then(result => {
 
-    res.json(hello);
+        if (result.count > 0) {
+            const rs = {
+                count: result.count,
+                classList: []
+            };
+            
+            result.rows.forEach(data => {
+                console.log(data.dataValues.timetable);
+                const jsonTimeTable = JSON.parse(data.dataValues.timetable);
+
+                console.log(jsonTimeTable);
+
+                rs.classList[data.id] = {
+                    name: data.dataValues.name,
+                    capacity: data.dataValues.capacity,
+                    levelId: data.dataValues.level_id,
+                    location: data.dataValues.location,
+                    timetable: jsonTimeTable,
+                    teacher: data.dataValues.teacher_id
+                };
+            });
+
+            console.log(rs);
+
+            res.status(200).json(rs);
+        } else {
+            res.status(204).json("No Contents");
+        }
+
+    }).catch(err => {
+        console.log(err);
+        res.status(555).json("DB ERROR");
+    })
 }
 
 function postClass(req, res) {
+
     const name = req.swagger.params.name.value || '';
     const capacity = req.swagger.params.capacity.value || '';
     const levelId = req.swagger.params.levelId.value || '';
     const location = req.swagger.params.location.value || '';
-    const timetable = req.swagger.params.timetable.value || '';
+    const timetable = JSON.parse(req.swagger.params.timetable.value) || '';
     const teacherId = req.swagger.params.teacherId.value || '';
 
-    console.log(req.swagger.params);
+    Class.findOrCreate({
+        where: {name: name},
+        defaults: {
+            capacity: capacity,
+            level_id: levelId,
+            location: location,
+            timetable: timetable,
+            teacher_id: teacherId
+        }
+    }).spread((newClass, created) => {
+        if (created) {
+            const result = {
+                success: true,
+                classId: newClass.dataValues.id
+            }
 
-    Class.create({
-        name: name,
-        capacity: capacity,
-        level_id: levelId,
-        location: location,
-        timetable: timetable,
-        teacher_id: teacherId
-    }).then((new_class) => res.status(201).json(new_class))
-    .catch((err) => console.error(err));    
+            res.status(200).json(result);
+        } else {
+            res.status(409).json("동일한 이름의 클래스가 존재합니다.");
+        }
+    }).catch((err) => {
+        res.status(555).json();
+    });
 }
--- api/controllers/teachers.js
+++ api/controllers/teachers.js
@@ -8,31 +8,31 @@ module.exports = {
 
 function createTeacher(req, res) {
     
-    // 이메일로 user_id 검색하여 있으면 생성
+    // 이메일로 user_id 검색하여 USER_TB에 있으면 생성
     User.findOne({
         where: {email: req.swagger.params.email.value},
         attributes: ['id']
     }).then(user => {
-        if (!user) {
-            console.log('없음');
-        } else {
-            console.log(user.dataValues.id);
 
-            Teacher.create({
-
-                user_id: user.dataValues.id,
-                level_id: req.swagger.params.levelId.value
-        
-            }).then((teacher) => {
-        
-                teacher.success = true;
-        
-                return res.json(teacher);
-            }).catch((err) => {
-                return res.status(555).json(false);
-            })
-        }
-
-        return res.status(200).json({success:true});
+        // TEACHER_TB 검색하여 없는 경우에만 생성
+        Teacher.findOrCreate({
+            where: {user_id: user.dataValues.id},
+            default: {
+                level_id:req.swagger.params.levelId.value
+            }
+        }).spread((teacher, created) => {
+            if (created) {
+                const result = {
+                    success: true,
+                    teacherId: teacher.dataValues.id                   
+                }
+                res.status(200).json(result);
+            } else {
+                console.log('1');
+                res.status(409).json("이미 등록된 선생님입니다.");
+            }
+        }).catch((err) => {
+            res.status(555).json(false);
+        });
     });
 }
